5cdf6a6eb07a1960d78fbb5c42432890079fec32
Alex Pécsi <xea@remalomfold.hu>
2014-11-24 19:40:53 +0000

    sandbox update

--- src/Sandbox/Sandbox.hs
+++ src/Sandbox/Sandbox.hs
@@ -1,5 +1,19 @@
-module Sandbox.Sandbox where
+module Main where
 
+import Control.Applicative
+import Data.Char
+
+class Test a where
+  (>>>) :: a -> a -> a 
+
+infixl 6 >>>
+
+instance Test Bool where
+  (>>>) a b = if (a == True) then b else False 
 
 main = do
-    putStrLn "Hello azz"
+  putStrLn $ show $ True >>> False
+  putStrLn $ show $ map reverse [ "Alma", "Korte", "Ananasz" ]
+  putStrLn "Hello azz"
+
+-- vim: et:sw=2:tw=2
