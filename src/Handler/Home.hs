d46df51016f159ba27b14cece7edbc509bfe54fb
Alex Pécsi <xea@remalomfold.hu>
2014-12-03 13:26:45 +0000

    minor upate

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -5,12 +5,12 @@ import Import
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
-    setTitle "Connect Prototype UI"
-    addScriptRemote "http://code.jquery.com/jquery-2.1.1.min.js"
-    addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"
-    addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"
-    [whamlet|
-        <div .container>
-             <h1>Connect Prototype UI
-    |]
+  setTitle "Connect Prototype UI"
+  addScriptRemote "http://code.jquery.com/jquery-2.1.1.min.js"
+  addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"
+  addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"
+  [whamlet|
+    <div .container>
+    <h1>Connect Prototype UI
+  |]
 -- vim: sw=2:ts=2:et
