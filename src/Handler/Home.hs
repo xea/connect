fcdbf5db369df1b8e4d74e98e954c1ffcf88ce93
Sandor Pecsi <specsi@mimecast.com>
2014-12-07 11:21:14 +0000

    Angular js and default layout

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,32 +3,23 @@ module Handler.Home where
 import Handler.Markdown
 import Settings.StaticFiles
 import Import
+import Text.Julius
+import Layout
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout $ do
+getHomeR = applicationLayout $ do
   setTitle "Connect Prototype UI"
   addScriptRemote "http://code.jquery.com/jquery-2.1.1.min.js"
   addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"
-  addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"
+  addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"
   addStylesheet $ StylesheetR "material.min.css"
   addStylesheet $ StylesheetR "ripples.min.css"
+  addScript $ JavascriptR "angular.min.js"
   addScript $ JavascriptR "material.min.js"
   addScript $ JavascriptR "ripples.min.js"
   addScript $ JavascriptR "application.js"
-  toWidget
-    [julius|
-      $(function() {
-      });
-    |]
-  [whamlet|
-    <div .container>
-      <div .navbar .navbar-warning>
-        <div .navbar-header>
-          <form .navbar-form .navbar-left>
-            <input .form-control .col-lg-8 type=text>
-      <div .alert .alert-dismissable .alert-info>
-        <button .close type=button data-dismiss=alert>
-        Fufufuufuf
-  |]
+  toWidgetHead $(juliusFileReload "templates/test.julius")
+  $(widgetFile "container")
+
 --    <img src=@{StaticR image_jpg }/>
 -- vim: sw=2:ts=2:et
--- static/javascripts/application.js
+++ static/javascripts/application.js
@@ -1,22 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-
 $(function() {
 	$.material.init();
+
+	 $(document).keypress(function(event) { if (event.which == 13) { event.preventDefault(); } });
+	 $(document).on("keyup", function(event) {
+		 if (event.which == 13) {
+			 event.preventDefault();
+		 }
+	 });
+
+	$("#ajax-input").keypress(function(event) {
+		if (event.which == 13) {
+			var url = "/data";
+			var request = {};
+			$.getJSON(url, request, function(response) {
+				
+			});
+		}
+	});
 });
 
-// vim: ts=4:sw=4:et
+// vim: sw=2:ts=2:noet
