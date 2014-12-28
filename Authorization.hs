933b9b2cce4340650839ae85995af5c732416a34
xea <alex.pecsi@gmail.com>
2014-12-28 19:30:02 +0100

    Pre-refactor cleaning

--- Authorization.hs
+++
@@ -1,7 +0,0 @@
-module Authorization where
-
-isAdmin = do
-  authId <- maybeAuthId
-  return $ case authId of
-    Nothing -> AuthenticationRequired
-    Just _ -> Unauthorized "Administrator privileges are required"
