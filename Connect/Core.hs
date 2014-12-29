ef397ff4ccff9b79a27aa12292b748c99d4fcd0f
xea <alex.pecsi@gmail.com>
2014-12-29 16:02:45 +0100

    find refactor

--- Connect/Core.hs
+++ Connect/Core.hs
@@ -9,6 +9,20 @@ data SkillNode = Collection { collectionElems :: [ SkillNode ] } |
                  Group { groupId :: String, groupItems :: [ SkillNode ] } |
                  Item { itemText :: String, answers :: [ String ] } deriving (Show, Read, Ord, Eq, Generic)
 
+data Node = Node { nodeId :: String, nodes :: [ Node ] }
+
+class Browsable a where
+  findN :: a -> [ SkillNode ]
+
+instance Browsable SkillNode where
+  findN c@(Collection _) = concat $ map (findItems) $ collectionElems c
+  findN g@(Group _ _) = concat $ map (findItems) $ groupItems g
+  findN i@(Item _ _) = [ i ]
+
+instance (Browsable a) => Browsable (Maybe a) where
+  findN Nothing = []
+  findN (Just a) = findN a
+
 -- | A "User" represents a real user of the software
 data User = User { userEmail :: String } deriving (Show, Read, Generic)
 
@@ -21,7 +35,25 @@ lookupRef ref coll@(Collection { collectionElems = celems }) = foldl max Nothing
 lookupRef ref _ = Nothing
 
 -- | The "findItems" collects all the "Item" nodes from the given tree
-findItems :: String -> SkillNode -> [ SkillNode ]
-findItems ref c@(Collection _) = concat $ map (findItems ref) $ collectionElems c
-findItems ref g@(Group _ _) = concat $ map (findItems ref) $ groupItems g
-findItems ref i@(Item _ _) = [ i ]
+findItems :: SkillNode -> [ SkillNode ]
+findItems c@(Collection _) = concat $ map (findItems) $ collectionElems c
+findItems g@(Group _ _) = concat $ map (findItems) $ groupItems g
+findItems i@(Item _ _) = [ i ]
+
+testData :: SkillNode
+testData = Collection 
+            [ Group "English" 
+              [ Group "Reading" 
+                [ Group "Vowels" 
+                  [ Item "A" [],
+                    Item "E" [],
+                    Item "I" []
+                  ],
+                  Group "Consonants"
+                  [ Item "K" [],
+                    Item "L" [] ] ] ],
+              Group "German" 
+                [ Group "Fachte" 
+                  [ Item "der Tisch" [],
+                    Item "der Mann" [] 
+                  ] ] ]
--- Connect/Store.hs
+++ Connect/Store.hs
@@ -3,3 +3,6 @@ module Connect.Store where
 import Connect.Core
 import Data.Aeson (decodeStrict)
 import Data.Aeson.Encode.Pretty (encodePretty)
+
+instance ToJSON SkillNode
+instance FromJSON SkillNode
